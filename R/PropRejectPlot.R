2d90f1721203f2d03994402add2874414daa9082
Adam Loy <loyad01@gmail.com>
2013-05-12 12:17:45 -0500

    Fixing the code for the plots

--- R/PropRejectPlot.R
+++ R/PropRejectPlot.R
@@ -13,6 +13,48 @@ library(ggplot2)
 ### Reading in the data
 propreject <- read.csv(file.choose()) # choose PropRejectSummary.csv
 
+#-------------------------------------------------------------------------------
+# Code for the current figures
+#-------------------------------------------------------------------------------
+
+# Summarizing results of AD tests for random intercept
+qplot(x = s, y = prop.reject, data = subset(propreject, subset = ranef == "norm" & nortest == "AD" &  alpha == 0.05 & random.effect == "b0" & var.settings == "sige2_sigb1" & rotation != "EBLUP"), geom = c("point", "smooth"), colour = error, group = error, linetype = error, se = F, method = "lm") + 
+	facet_grid(. ~ rotation, scales = "free") + 
+	xlab("s") +
+	ylab("proportion of tests rejected") + 
+	scale_linetype_discrete("Error distribution", labels = c("Skewed", "Normal", "Heavy tailed")) + 	scale_color_discrete("Error distribution", labels = c("Skewed", "Normal", "Heavy tailed"))
+
+# Summarizing results of AD tests for random slope
+qplot(x = s, y = prop.reject, data = subset(propreject, subset = ranef == "norm" & nortest == "AD" &  alpha == 0.05 & random.effect == "b1" & var.settings == "sige2_sigb1" & rotation != "EBLUP"), geom = c("point", "smooth"), colour = error, group = error, linetype = error, se = F, method = "lm") + 
+	facet_grid(. ~ rotation, scales = "free") + 
+	xlab("s") +
+	ylab("proportion of tests rejected") + 
+	scale_linetype_discrete("Error distribution", labels = c("Skewed", "Normal", "Heavy tailed")) + 	scale_color_discrete("Error distribution", labels = c("Skewed", "Normal", "Heavy tailed")) + 
+	ylim(0, .4)
+
+### Plotting the power
+# Summarizing results of AD tests for random intercept
+qplot(x = s, y = prop.reject, data = subset(propreject, subset = ranef != "norm" & random.effect == "b0" & nortest == "AD" & var.settings == "sige2_sigb1" & alpha == 0.05 & rotation != "EBLUP"), geom = c("point", "smooth"), facets = ~ rotation, linetype = ranef, colour = error, group = interaction(error, ranef), se = F, method = "lm") + 
+	xlab("s") + 
+	ylab("proportion of tests rejected") + 
+	theme_bw() + 
+	scale_color_discrete("Error distribution", labels = c("Skewed", "Normal", "Heavy tailed")) + 
+	scale_linetype_discrete("Random effects\ndistribution", labels = c("Skewed", "Heavy tailed"))
+
+# Summarizing results of AD tests for random slopes
+qplot(x = s, y = prop.reject, data = subset(propreject, subset = ranef != "norm" & random.effect == "b1" & nortest == "AD" & var.settings == "sige2_sigb1" & alpha == 0.05 & rotation != "EBLUP"), geom = c("point", "smooth"), facets = ~ rotation, colour = error, linetype = ranef, group = interaction(error, ranef), se = F, method = "lm") + 
+	xlab("s") + 
+	ylab("proportion of tests rejected") + 
+	theme_bw() + 
+	scale_color_discrete("Error distribution", labels = c("Skewed", "Normal", "Heavy tailed")) + 
+	scale_linetype_discrete("Random effects\ndistribution", labels = c("Skewed", "Heavy tailed"))
+
+
+
+#-------------------------------------------------------------------------------
+# Trying to add in a third panel for the EBLUPs
+#-------------------------------------------------------------------------------
+
 propreject$rotation <- factor(propreject$rotation, levels = levels(propreject$rotation)[c(2,3,1)])
 propreject$s <- factor(propreject$s)
 
